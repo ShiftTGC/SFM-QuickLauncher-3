9e44434c0765b57df00c393d2d3a546a9b179ffd
ShiftTGC <ShiftTGC@gmail.com>
2024-02-29 18:39:05 +0100

    Sorted Parsing stuff and Search stuff into seperate file things and some minor preperations for saving dynamic data (SFM path, custom user-defined launch parameters and a toggle for "always offline/online")

---
+++ SFM Location Retriever/Parsing.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SFM_Location_Retriever
+{
+    public class Parsing
+    {
+
+        /// <summary>
+        /// Goes through and checks if the paths actually exists, removes them if they don't. (Could be an external drive with a Steam library has disconected or something)
+        /// </summary>
+        /// <param name="steamGameDirs"></param>
+        /// <returns></returns>
+        public static List<string> RemoveInvalidPaths(List<string> steamGameDirs)
+        {
+            List<int> indexList = new List<int>();
+            int index = 0;
+            foreach (string steamGameDir in steamGameDirs)
+            {
+                Console.Write(steamGameDir);
+                if (Directory.Exists(steamGameDir))
+                {
+                    Console.WriteLine("  //Is real");
+                }
+                else
+                {
+                    Console.WriteLine("  //Doesn't exist");
+                    indexList.Add(index);
+                }
+                index++;
+            }
+
+            //Stolen from https://stackoverflow.com/a/9908607
+            foreach (int index2 in indexList.OrderByDescending(v => v))
+            {
+                Console.WriteLine($"Removing: {steamGameDirs[index2]}");
+                steamGameDirs.RemoveAt(index2);
+            }
+            return steamGameDirs;
+
+        }
+
+        /// <summary>
+        /// Finds the location(s) of the Source Filmmaker folder(s) from the Steam Library path(s) with unecesary debug text printed to the console. Fun clutter imo.
+        /// </summary>
+        /// <param name="steamGameDirs">Contains all Steam Library Paths</param>
+        /// <returns></returns>
+        public static List<string> FindSFM(List<string> steamGameDirs)
+        {
+            List<string> sourceFilmmakerPaths = new List<string>();
+
+            foreach (var steamGameDir in steamGameDirs)
+            {
+                Console.Write(steamGameDir);
+                if (Directory.Exists(steamGameDir + "SourceFilmmaker"))
+                {
+                    Console.WriteLine("SourceFilmmaker     //IT IS HEEEERE! WOOOOOOO!");
+                    sourceFilmmakerPaths.Add(steamGameDir + "SourceFilmmaker");
+                }
+                else
+                {
+                    Console.WriteLine("[N/A]     //No SFM");
+                }
+
+            }
+            return sourceFilmmakerPaths;
+        }
+    }
+}
--- SFM Location Retriever/Program.cs
+++ SFM Location Retriever/Program.cs
@@ -13,9 +13,11 @@ namespace SFM_Location_Retriever
 
         static void Main(string[] args)
         {
+            bool configExists = CheckConfigFile();
+
             Logo(); //Displays Logo
 
-            //Console.ReadKey();
+            Console.ReadKey();
 
             Console.WriteLine("\nThere will be a check to see if SFM is already discovered added later\n");
 
@@ -23,15 +25,13 @@ namespace SFM_Location_Retriever
             List<string> sourceFilmmakerPaths = new List<string>();
             List<string> steamGameDirs = new List<string>();
 
-            Console.WriteLine(sourceFilmmakerPaths.Count());
-
             steamGameDirs.Add("This is a test removal number"); //Gets removed during "SearchSteam"
             
-            steamGameDirs = SearchSteam(); //Retrieves the paths of libraries
+            steamGameDirs = Search.SearchSteam(); //Retrieves the paths of libraries
 
             steamGameDirs.Add("This is a test removal number 2"); //Debugging purposes. Lets me verify the remover works as it is intended to do
 
-            sourceFilmmakerPaths = FindSFM(RemoveInvalidPaths(steamGameDirs));
+            sourceFilmmakerPaths = Parsing.FindSFM(Parsing.RemoveInvalidPaths(steamGameDirs));
 
 
             switch (sourceFilmmakerPaths.Count())
@@ -50,151 +50,20 @@ namespace SFM_Location_Retriever
                     Console.WriteLine(sourceFilmmakerPaths[0] + "\\game\\sfm.exe");
                     break;
             }
-            
-            Console.WriteLine(sourceFilmmakerPaths.Count());
-
         }
 
         /// <summary>
-        /// Goes through and checks if the paths actually exists, removes them if they don't. (Could be an external drive with a Steam library has disconected or something)
-        /// </summary>
-        /// <param name="steamGameDirs"></param>
-        /// <returns></returns>
-        private static List<string> RemoveInvalidPaths(List<string> steamGameDirs)
-        {
-            List<int> indexList = new List<int>();
-            int index = 0;
-            foreach (string steamGameDir in steamGameDirs)
-            {
-                Console.Write(steamGameDir);
-                if (Directory.Exists(steamGameDir))
-                {
-                    Console.WriteLine("  //Is real");
-                }
-                else
-                {
-                    Console.WriteLine("  //Doesn't exist");
-                    indexList.Add(index);
-                }
-                index++;
-            }
-
-            //Stolen from https://stackoverflow.com/a/9908607
-            foreach (int index2 in indexList.OrderByDescending(v => v))
-            {
-                Console.WriteLine($"Removing: {steamGameDirs[index2]}");
-                steamGameDirs.RemoveAt(index2);
-            }
-            return steamGameDirs;
-            
-        }
-
-        /// <summary>
-        /// Finds the location(s) of the Source Filmmaker folder(s) from the Steam Library path(s) with unecesary debug text printed to the console. Fun clutter imo.
-        /// </summary>
-        /// <param name="steamGameDirs">Contains all Steam Library Paths</param>
-        /// <returns></returns>
-        private static List<string> FindSFM(List<string> steamGameDirs)
-        {
-            List<string> sourceFilmmakerPaths = new List<string>();
-
-            foreach (var steamGameDir in steamGameDirs)
-            {
-                Console.Write(steamGameDir);
-                if (Directory.Exists(steamGameDir + "SourceFilmmaker"))
-                {
-                    Console.WriteLine("SourceFilmmaker     //IT IS HEEEERE! WOOOOOOO!");
-                    sourceFilmmakerPaths.Add(steamGameDir + "SourceFilmmaker");
-                }
-                else
-                {
-                    Console.WriteLine("[N/A]     //No SFM");
-                }
-
-            }
-            return sourceFilmmakerPaths;
-        }
-
-        /// <summary>
-        /// Stolen from https://stackoverflow.com/a/54886330 and modified to work for what I needed
+        /// 
         /// </summary>
         /// <returns></returns>
-        public static List<string> SearchSteam()
+        private static bool CheckConfigFile() //There is supposedly a better way to make/deal with configuratins etc, buuuuut, this seems easier
         {
-            List<string> steamGameDirs = new List<string>();
-            steamGameDirs.Clear();
-            string steam32 = "SOFTWARE\\VALVE\\";
-            string steam64 = "SOFTWARE\\Wow6432Node\\Valve\\";
-            string steam32path;
-            string steam64path;
-            string config32path;
-            string config64path;
-            RegistryKey key32 = Registry.LocalMachine.OpenSubKey(steam32);
-            RegistryKey key64 = Registry.LocalMachine.OpenSubKey(steam64);
-            if (key64.ToString() == null || key64.ToString() == "") //Checks if it is a 32-bit or 64-bit System
-            {
-                foreach (string k32subKey in key32.GetSubKeyNames())
-                {
-                    using (RegistryKey subKey = key32.OpenSubKey(k32subKey))
-                    {
-                        try { steam32path = subKey.GetValue("InstallPath").ToString(); }
-                        catch { return steamGameDirs; }
-                        config32path = steam32path + "/steamapps/libraryfolders.vdf";
-                        string driveRegex = @"[A-Z]:\\";
-                        if (File.Exists(config32path))
-                        {
-                            string[] configLines = File.ReadAllLines(config32path);
-                            foreach (var item in configLines)
-                            {
-                                Console.WriteLine("32:  " + item + "    //Enjoy the debug text I forgot to disable");
-                                Match match = Regex.Match(item, driveRegex);
-                                if (item != string.Empty && match.Success)
-                                {
-                                    string matched = match.ToString();
-                                    string item2 = item.Substring(item.IndexOf(matched));
-                                    item2 = item2.Replace("\\\\", "\\");
-                                    item2 = item2.Replace("\"", "\\steamapps\\common\\");
-                                    steamGameDirs.Add(item2);
-                                    steamGameDirs.Add("This is a test removal number 2"); //Debugging purposes. Lets me verify the remover works as it is intended to do
-                                    Console.WriteLine($"{matched} {item2}   //Library folder");
-                                }
-                            }
-                        }
-                    }
-                }
-                return steamGameDirs;
-            }
-            foreach (string k64subKey in key64.GetSubKeyNames())
-            {
-                using (RegistryKey subKey = key64.OpenSubKey(k64subKey))
-                {
-                    try { steam64path = subKey.GetValue("InstallPath").ToString(); }
-                    catch { return steamGameDirs; }
-                    config64path = steam64path + "/steamapps/libraryfolders.vdf";
-                    string driveRegex = @"[A-Z]:\\";
-                    if (File.Exists(config64path))
-                    {
-                        string[] configLines = File.ReadAllLines(config64path);
-                        foreach (var item in configLines)
-                        {
-                            Console.WriteLine("64:  " + item + "     //Enjoy the debug text I forgot to disable");
-                            Match match = Regex.Match(item, driveRegex);
-                            if (item != string.Empty && match.Success)
-                            {
-                                string matched = match.ToString();
-                                string item2 = item.Substring(item.IndexOf(matched));
-                                item2 = item2.Replace("\\\\", "\\");
-                                item2 = item2.Replace("\"", "\\steamapps\\common\\");
-                                steamGameDirs.Add(item2);
-                                steamGameDirs.Add("This is a test removal number 2"); //Debugging purposes. Lets me verify the remover works as it is intended to do
-                                Console.WriteLine($"{matched} {item2}   //Library folder");
+            string appDataLocal = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string configFileLocation = "ShiftTGC\\SFM-Quicklancher-3\\settings.config";
 
-                            }
-                        }
-                    }
-                }
-            }
-            return steamGameDirs;
+
+
+            return false;
         }
 
         static void Logo()
